43030bb01b7dd03eee6776a506fd85ff51b61a97
heyadjoining <adjoiningworld@gmail.com>
2021-10-22 10:11:04 -0500

    Timer and upload rarity images

--- js/index.js
+++ js/index.js
@@ -31,6 +31,34 @@ const preloadFonts = () => {
     });
 };
 
+//Timer function
+(function () {
+    const second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24;
+
+    let future = new Date('2021-10-23T22:00:00+00:00');
+    
+    const countDown = new Date(future).getTime(),
+        x = setInterval(function() {    
+            
+            const now = new Date().getTime(),
+                distance = countDown - now;
+            
+            document.getElementById("days").innerText = Math.floor(distance / (day)),
+            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+            
+            if (distance < 0) {
+            document.getElementById("countdown").style.display = "none";
+            document.getElementById("content").style.display = "block";
+            clearInterval(x);
+            }
+        }, 0)
+    }());
+
 // Preload fonts and images
 Promise.all([preloadImages(), preloadFonts()]).then(() => {
     // And then initialize the TextOnScroll instances
